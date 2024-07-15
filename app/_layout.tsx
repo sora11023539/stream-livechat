7c309bb9af20aa504297fe0c6f20824e5957a302
sora11023539 <sora11023539@gmail.com>
2024-07-15 23:11:32 +0900

    Styling Login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,22 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import "react-native-gesture-handler"
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { Stack } from "expo-router"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+const InitialLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
+
+const RootLayoutNav = () => {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <InitialLayout />
+    </GestureHandlerRootView>
+  )
+}
+
+export default RootLayoutNav
---
+++ app/index.tsx
@@ -0,0 +1,98 @@
+import {
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  Dimensions
+} from 'react-native'
+import React, { useState } from 'react'
+import Spinner from 'react-native-loading-spinner-overlay'
+import Colors from '@/constants/Colors'
+
+const WIDTH = Dimensions.get('window').width
+const HEIGHT = Dimensions.get('window').height
+
+const Page = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const onSignInPress = async () => {
+  }
+
+  const onSignUpPress = async () => {
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <Spinner visible={loading} />
+      <Text style={styles.header}>Meet Me</Text>
+      <Text style={styles.subHeader}>The fastest way to meet</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder='Password'
+        style={styles.inputField}
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+        <Text style={{ color: "#fff" }}>Sign In</Text>
+      </TouchableOpacity>
+      <Button
+        title="Don't have an account? Sign UP"
+        onPress={onSignUpPress}
+        color={Colors.primary}
+      />
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingHorizontal: WIDTH > HEIGHT ? '30%' : 20,
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  subHeader: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderColor: Colors.primary,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+  },
+  button: {
+    marginVertical: 15,
+    alignItems: 'center',
+    backgroundColor: Colors.primary,
+    padding: 12,
+    borderRadius: 4,
+  }
+})
+
+export default Page
